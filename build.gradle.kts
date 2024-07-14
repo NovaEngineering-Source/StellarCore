9a7ca9f92c4a05d08de8b647469b7f398ab038a3
Hikari_Nova <3044344887@qq.com>
2024-07-15 01:32:21 +0800

    - 修复一个错误的判断！

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 // Project properties
 group = "github.kasuminova.stellarcore"
-version = "1.0.27"
+version = "1.0.28"
 
 // Set the toolchain version to decouple the Java we run Gradle with from the Java used to compile and run the mod
 java {
