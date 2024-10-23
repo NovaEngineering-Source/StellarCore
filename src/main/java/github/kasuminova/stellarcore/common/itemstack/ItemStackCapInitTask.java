e0e9440e2398507ece025358ba4293b68591c937
Hikari_Nova <3044344887@qq.com>
2024-10-23 09:21:34 +0800

    - ItemStackCapInitTask 边界情况异常修复。

--- src/main/java/github/kasuminova/stellarcore/common/itemstack/ItemStackCapInitTask.java
+++ src/main/java/github/kasuminova/stellarcore/common/itemstack/ItemStackCapInitTask.java
@@ -4,11 +4,18 @@ import github.kasuminova.stellarcore.common.util.StellarLog;
 import github.kasuminova.stellarcore.mixin.util.StellarItemStack;
 import net.minecraft.item.ItemStack;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @SuppressWarnings("DataFlowIssue")
 public class ItemStackCapInitTask implements Runnable {
 
     private final StellarItemStack target;
-    private volatile boolean done = false;
+
+    private final AtomicBoolean done = new AtomicBoolean(false);
+    private final AtomicBoolean joined = new AtomicBoolean(false);
+
+    private volatile Thread working = null;
+    private volatile Thread joining = null;
 
     public ItemStackCapInitTask(final ItemStack target) {
         this.target = (StellarItemStack) (Object) target;
@@ -16,26 +23,65 @@ public class ItemStackCapInitTask implements Runnable {
 
     @Override
     public synchronized void run() {
-        if (done) {
+        if (done.get()) {
             return;
         }
+        working = Thread.currentThread();
+
         try {
             target.stellar_core$initCap();
         } catch (Throwable e) {
             StellarLog.LOG.warn("[StellarCore-ItemStackCapInitTask] Failed to execute capability init task!", e);
         }
-        done = true;
+
+        working = null;
+        done.set(true);
     }
 
     public boolean isDone() {
-        return done;
+        return done.get();
     }
 
-    public void join() {
-        if (!done) {
+    public boolean join() {
+        if (joined.get()) {
+            return true;
+        }
+
+        final Thread current = Thread.currentThread();
+        // Recursion check.
+        if (this.joining == current || this.working == current) {
+            return false;
+        }
+
+        // Lock the target instead of the task itself.
+        synchronized (this) {
+            // Recursion check.
+            if (this.joining == current || this.working == current) {
+                return false;
+            }
+            // Wait for another thread finish.
+            awaitJoinComplete();
+            // Set the joining thread.
+            this.joining = current;
+        }
+
+        if (!done.get()) {
             run();
         }
-        target.stellar_core$joinCapInit();
+        if (!joined.get()) {
+            target.stellar_core$joinCapInit();
+            joined.set(true);
+        }
+
+        this.joining = null;
+        return true;
+    }
+
+    private void awaitJoinComplete() {
+        // Wait for the task to finish.
+        while (this.joining != null) {
+            Thread.yield();
+        }
     }
 
 }
